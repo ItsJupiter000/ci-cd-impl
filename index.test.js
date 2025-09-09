0c98400bdae0e267cfb89f07332acbe4eecc5e7a
Uttam Gohil <itsjupiter000@gmail.com>
2025-09-09 23:50:49 +0530

    ci: add github actions update

--- index.test.js
+++ index.test.js
@@ -1,8 +1,8 @@
 const request = require("supertest");
 const app = require("./index");
 
-test("GET / should return Hello CI/CD World!", async () => {
+test("GET / should return hello with app name", async () => {
   const res = await request(app).get("/");
   expect(res.statusCode).toBe(200);
-  expect(res.text).toBe("Hello CI/CD World!");
+  expect(res.text).toBe(`Hello from ${process.env.APP_NAME}!`);
 });
